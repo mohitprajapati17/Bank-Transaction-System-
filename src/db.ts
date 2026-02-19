ddda268656de8ecc3d7d5c18fb0651b6f6f8f0b9
mohitprajapati17 <mohitprajapati2444@gmail.com>
2026-02-19 22:02:09 +0530

    fix

--- src/db.ts
+++ src/db.ts
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://mohitprajapati2444_db_user:3ONMYX7CJ6eDohBg@127.0.0.1:3306/bank_transaction_db");
+        await mongoose.connect(process.env.MONGO_URI || "");
         console.log("MongoDB connected");
     } catch (error) {
         console.log("MongoDB connection error", error);
