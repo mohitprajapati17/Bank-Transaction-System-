715008bad7444dea5ab2bc1e85cc128cd87c9c37
mohitprajapati17 <mohitprajapati2444@gmail.com>
2026-02-19 22:53:08 +0530

    add env

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
-import app from "./src/app";
+import app from "./src/app.ts";
+import connectDB from "./src/db.ts";
 
 const PORT =3000;
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,14 @@
 // 3ONMYX7CJ6eDohBg
 // mohitprajapati2444_db_user
+import dotenv from "dotenv";
+dotenv.config();
 
 import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI || "");
+
         console.log("MongoDB connected");
     } catch (error) {
         console.log("MongoDB connection error", error);
