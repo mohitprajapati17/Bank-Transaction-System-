340d28cba0a3f408626ee2bfcbc7969d17bf79d0
mohitprajapati17 <mohitprajapati2444@gmail.com>
2026-02-25 23:57:12 +0530

    hash

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcrypt";
 const userSchema = new mongoose.Schema({
     email :{
         type : String,
@@ -21,8 +21,21 @@ const userSchema = new mongoose.Schema({
         select:false,
     },
     systemUser:{
-        type:Boolean
+        type:Boolean,
+        default :false,
+        immutable:true,
+        select:false,
     }
+},{
+    timestamps:true,
+})
+
+userSchema.pre("save", async function(){
+    if(this.isModified("password")){
+        return;
+    }
+    const hash=await bcrypt.hash(this.password, 10);
+    this.password = hash;
 })
 
 export const User = mongoose.model("User", userSchema)
