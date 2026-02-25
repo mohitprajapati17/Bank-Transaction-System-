4c198c9c7e85f95b2849105d636932476d6b2cec
mohitprajapati17 <mohitprajapati2444@gmail.com>
2026-02-25 23:39:08 +0530

    creating model user

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,9 +1,27 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name :{
+    email :{
         type : String,
-        required : true
+        required : [true,"Email is required for creating user "],
+        trim:true,
+        lowercase:true,
+        unique:[true,"Email already exists"],
+        match:[ /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid Email address" ],
+    },
+    name:{
+        type:String,
+        required:[true,"Name is required for creating user"],
+
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required for creating user"],
+        minlength:[6,"Password must be at least 6 characters long"],
+        select:false,
+    },
+    systemUser:{
+        type:Boolean
     }
 })
 
