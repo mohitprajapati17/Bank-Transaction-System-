e726a5d309743a1d9c6ea5f1cc73ca271440775a
mohitprajapati17 <mohitprajapati2444@gmail.com>
2026-02-27 23:18:58 +0530

    routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from "express";
-
+import authrouter from "./routes/auth.routes";
 const app=express()
+app.use(express.json());
+
+app.use("/api/auth", authrouter);
+
+
 
 export default app;
---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,6 @@
+import Router from "express";
+const authrouter = Router();
+
+
+
+export default authrouter;
