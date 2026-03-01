20da09e34c28bd111cb5d2a99aa04703f0151b56
mohitprajapati17 <mohitprajapati2444@gmail.com>
2026-03-01 23:58:27 +0530

    controller fix

--- src/controllers/authcontroller.ts
+++ src/controllers/authcontroller.ts
@@ -6,8 +6,19 @@ import { Request, Response } from "express";
  * -POST /api/auth/register
  */
 
-export function userRegisterController(req:Request, res:Response){
-    const {email,password} = req.body;
+export async function userRegisterController(req:Request, res:Response){
+    const {email,password,name} = req.body;
+
+
+    const isexist = await userModel.findOne({email});
+    if(isexist){
+        return res.status(400).json({message:"User already exists"});
+    }
+    await userModel.create({email,password,name});
+
+    return res.status(200).json({message:"User registered successfully"});
+
+
 
 }
 
