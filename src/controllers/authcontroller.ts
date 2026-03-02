727c111ebd19e9109776e9c03c0afba5a873e42d
mohitprajapati17 <mohitprajapati2444@gmail.com>
2026-03-02 20:10:00 +0530

    json-web-token

--- src/controllers/authcontroller.ts
+++ src/controllers/authcontroller.ts
@@ -9,16 +9,13 @@ import { Request, Response } from "express";
 export async function userRegisterController(req:Request, res:Response){
     const {email,password,name} = req.body;
 
-
     const isexist = await userModel.findOne({email});
     if(isexist){
-        return res.status(400).json({message:"User already exists"});
+        return res.status(402).json({message:"User already exists",status:"failed"});
     }
-    await userModel.create({email,password,name});
+    const user=await userModel.create({email,password,name});
 
-    return res.status(200).json({message:"User registered successfully"});
-
-
+    return res.status(200).json({message:"User registered successfully",status:"success",user});
 
 }
 
